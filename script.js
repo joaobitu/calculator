d72deaf392f10cc4a038e70e97182c4167f51011
joaobitu <joaovbitu@gmail.com>
2022-01-09 14:29:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+let numberValue;
+let operatorValue;
+
+
+const displayArea = document.querySelector("#visor");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector("#clear");
+const equalButton = document.querySelector("#equal");
+
+function add (a, b){
+    return a + b;
+}
+function subtract (a, b) {
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a,b){
+    return Math.round(a / b * 100) / 100;
+}
+
+function operator(operator, a, b){
+    if (operator == "+"){
+       return add(a, b);
+    } else if (operator == "-"){
+       return subtract(a, b);
+    } else if (operator == "*"){
+       return multiply(a, b);
+    } else if (operator == "/"){
+        return divide(a, b);
+    } 
+    console.log("Unsupported parameter");
+}
+
+numberButtons.forEach((x) => x.addEventListener('click', () =>{
+    displayArea.textContent += x.value;
+    return x.value;
+ }))
+
+ operatorButtons.forEach((x) => x.addEventListener('click', () =>{
+    if (x.value == 10){
+        displayArea.textContent += " " + "+" + " ";
+        operatorValue = "+";
+    } else if (x.value == 11){
+        displayArea.textContent += " " + "-" + " ";
+        operatorValue = "-";
+    } else if (x.value == 12){
+        displayArea.textContent += " " + "*" + " ";
+        operatorValue = "*";
+    } else if (x.value == 13){
+        displayArea.textContent += " " + "/" + " ";
+        operatorValue = "/";
+    }   
+    
+    return x.value;
+ }))
+
+ clearButton.addEventListener('click', () => {
+        displayArea.textContent = "";
+ });
+
+ equalButton.addEventListener('click', () =>{
+        let FirstNumber = Number(displayArea.textContent.slice(0,displayArea.textContent.indexOf(" ")));
+        let SecondNumber = Number(displayArea.textContent.slice(displayArea.textContent.indexOf(" ") + 2));
+        console.log(FirstNumber);
+        console.log(SecondNumber);
+        if (operatorValue == "+"){
+            displayArea.textContent = add(FirstNumber, SecondNumber);
+        } else if (operatorValue == "-"){
+            displayArea.textContent = subtract(FirstNumber, SecondNumber);
+        } else if (operatorValue == "*"){
+            displayArea.textContent = multiply(FirstNumber, SecondNumber);
+        } else if (operatorValue == "/"){
+            if(SecondNumber == 0){
+                return displayArea.textContent = "ERROR";
+            }
+            displayArea.textContent = divide(FirstNumber, SecondNumber);
+        } else {
+            displayArea.textContent = "ERROR";
+        }
+ });
+
+
